0d860242f7e0fb2e1d16ca9f2514c166a6570648
Felipe Romero <feliperomero011@gmail.com>
2015-12-02 10:18:22 -0700

    Agregar pruebas unitarias.

--- MyOpenXmlTests/MySpreadsheetTest.vb
+++ MyOpenXmlTests/MySpreadsheetTest.vb
@@ -12,4 +12,31 @@ Public Class MySpreadsheetTest
         Assert.IsTrue(File.Exists(filePath))
     End Sub
 
+    <TestMethod()>
+    Public Sub DontCreateIfFilePathIsInvalid()
+        Dim filePath = "c:\\\Users\Public\Documents\Libro2.xlsx"
+        Dim spreadSheet = New MySpreadsheet()
+        spreadSheet.Create(filePath)
+        Assert.IsTrue(File.Exists(filePath))
+        'Dim isValidFilePath = Directory.Exists(Path.GetDirectoryName(filePath))
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub DontCreateIfFileExtensionIsInvalid()
+        Dim filePath = "c:\\Users\Public\Documents\Libro3"
+        Dim fileName = Path.GetFileName(filePath)
+        Dim spreadSheet = New MySpreadsheet()
+        spreadSheet.Create(filePath)
+        Assert.IsFalse(File.Exists(filePath), "fileName is:" & fileName)
+    End Sub
+
+    <TestMethod()>
+    <ExpectedException(GetType(DirectoryNotFoundException))>
+    Public Sub InvalidFileNameShouldThrowException()
+        Dim filePath = "c:\\Users\Public\Documents\"
+        Dim spreadSheet = New MySpreadsheet()
+        spreadSheet.Create(filePath)
+    End Sub
+
 End Class
