f6495124437aef89c4a3aeed795c1ee1b917d2c3
Felipe Romero <feliperomero011@gmail.com>
2015-12-01 18:05:27 -0700

    Crear CreateSpreadsheet() UnitTestMethod.

--- MyOpenXmlTests/MySpreadsheetTest.vb
+++ MyOpenXmlTests/MySpreadsheetTest.vb
@@ -1,3 +1,15 @@
-﻿Public Class MySpreadsheetTest
+﻿Imports MyOpenXml
+Imports System.IO
+
+<TestClass()>
+Public Class MySpreadsheetTest
+
+    <TestMethod()>
+    Public Sub CreateSpreadsheet()
+        Dim filePath = "c:\Users\Public\Documents\Libro1.xlsx"
+        Dim spreadSheet = New MySpreadsheet()
+        spreadSheet.Create(filePath)
+        Assert.IsTrue(File.Exists(filePath))
+    End Sub
 
 End Class
