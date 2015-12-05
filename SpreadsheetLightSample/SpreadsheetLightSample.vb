14dfa817e07b8543a5f91b916f498d139b780bb9
Felipe Romero <feliperomero011@gmail.com>
2015-12-05 13:15:07 -0700

    Agregar proyecto Spread

---
+++ SpreadsheetLightSample/SpreadsheetLightSample.vb
@@ -0,0 +1,9 @@
+﻿Imports SpreadsheetLight
+
+Public Class SpreadsheetLightSample
+    Public Shared Sub ExportDatatableToExcel(datatable As DataTable)
+        Dim document = New SLDocument()
+        document.ImportDataTable("A1", datatable, True)
+        document.SaveAs("c:\Users\Public\Documents\WriteDatabaseDataToSpreadsheetSL.xlsx")
+    End Sub
+End Class
